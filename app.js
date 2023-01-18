a1959f57083fbddcc240cc058a113db1a5869a2f
Joao <joao.robertofg@gmail.com>
2023-01-18 03:37:10 -0300

    page

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hidden, .ap');
 hiddenElements.forEach((el) => observer.observe(el));
