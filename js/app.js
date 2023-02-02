4de4ea5f4ef1f20517115f86dcb0f6466f262a83
Joao <joao.robertofg@gmail.com>
2023-02-02 05:03:57 -0300

    General

---
+++ js/app.js
@@ -0,0 +1,45 @@
+//fade-in
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden, .ap, .hidden-conhecimentos, .certificados');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//menu lateral
+
+const botaoMenu = document.querySelector('.cabecalho-menu');
+const menu = document.querySelector(".menu-lateral");
+const hideMenu = document.querySelector('.menu-link');
+const hideMenu2 = document.querySelector('.menu-link2');
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral-ativo')
+})
+
+//esconder menu lateral
+const listaMenu = document.querySelectorAll('.menu-link');
+
+listaMenu[0].addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral-ativo')
+})
+
+listaMenu[1].addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral-ativo')
+})
+
+listaMenu[2].addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral-ativo')
+})
+
+listaMenu[3].addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral-ativo')
+})
